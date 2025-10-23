7d7b1241ff6c52999bac96156420e88772bb7939
tebbe21485 <157887121+tebbe21485@users.noreply.github.com>
2025-10-23 14:42:17 -0500

    Did some testing and added documentation.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/paths/testPath.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/paths/testPath.java
@@ -12,10 +12,7 @@ public final class testPath {
     private testPath() {}
 
     public static final List<Pose2d> PATH = Arrays.asList(
-    new Pose2d(62.76, -8.91, new Rotation2d(3.14)),  // #1  x=62.76in, y=-8.91in, θ=0.0°
-    new Pose2d(5.00, -9.00, new Rotation2d(3.14)),  // #2  x=5.00in, y=-9.00in, θ=0.0°
-    new Pose2d(-23.64, 23.89, new Rotation2d(3.14)),  // #3  x=-23.64in, y=23.89in, θ=0.0°
-    new Pose2d(38.50, 33.50, new Rotation2d(3.14)),  // #4  x=38.50in, y=33.50in, θ=0.0°
-    new Pose2d(38.50, -33.00, new Rotation2d(3.14))  // #5  x=38.50in, y=-33.00in, θ=0.0°
+    new Pose2d(15, 15, new Rotation2d(3.14)),  // #1  x=62.76in, y=-8.91in, θ=0.0°
+    new Pose2d(0, 0, new Rotation2d(0))  // #2  x=5.00in, y=-9.00in, θ=0.0°
 );
 }
